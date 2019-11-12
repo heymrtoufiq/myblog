61b498b1b0cef67d06348691154f08c2ac3b9121
Toufiqur Rahman <toufiq@Toufiqurs-iMac.local>
2019-11-12 13:00:52 +0600

    404 page changed

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 		<div class="maincontent clear">
 			<div class="about">
 				<div class="notfound">
-    				<p><span>404</span> Not Found</p>
+    				<p><span>404</span> Not Found Anyting!</p>
     			</div>
 	        </div>
 		</div>
